03b83babb5be354621875ce9e4cdf61fe8bb5ccc
rbarrientos182 <barrientos.isc@gmail.com>
2016-11-28 18:39:43 -0600

    creación de migraciones para db

---
+++ database/migrations/2016_11_29_003513_create_historialclinico_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistorialclinicoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
