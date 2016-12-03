1de45bd653c03d583976d834700ba2185d4c236e
rbarrientos182 <barrientos.isc@gmail.com>
2016-12-02 19:13:19 -0600

    creación de plantillas blade

--- app/Usuarios.php
+++ app/Usuarios.php
@@ -2,9 +2,25 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Usuarios extends Model
+class Usuarios extends Authenticatable
 {
-    //
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+      'correo', 'password', 'activo',
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+      'password', 'remember_token',
+  ];
 }
