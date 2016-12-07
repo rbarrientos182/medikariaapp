b5bce68299c282201cfaa960bf087af3771dcc41
rbarrientos182 <barrientos.isc@gmail.com>
2016-12-06 18:50:51 -0600

    creación de modelos con nombre singular

---
+++ app/HistorialPago.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Medikaria;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HistorialPago extends Model
+{
+    //
+}
---
+++ app/Hospital.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Medikaria;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hospital extends Model
+{
+    //
+}
