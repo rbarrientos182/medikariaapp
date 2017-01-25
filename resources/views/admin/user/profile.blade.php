88eaa59f5c77232a938c1b980581154e8ce2afb2
rbarrientos182 <barrientos.isc@gmail.com>
2017-01-25 16:49:11 -0600

    módulo para ajuste de profile creado

--- resources/views/admin/user/profile.blade.php
+++ resources/views/admin/user/profile.blade.php
@@ -22,25 +22,25 @@
             <!-- Profile Image -->
             <div class="box box-primary">
               <div class="box-body box-profile">
-                <img class="profile-user-img img-responsive img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+                <img class="profile-user-img img-responsive img-circle" src="{{ asset('components/lte/img/userRoberto.jpg') }}" alt="User profile picture">
 
-                <h3 class="profile-username text-center">Nina Mcintire</h3>
+                <h3 class="profile-username text-center">{{ $currentUser->nombre }}</h3>
 
-                <p class="text-muted text-center">Software Engineer</p>
+                <p class="text-muted text-center">Médico</p>
 
                 <ul class="list-group list-group-unbordered">
                   <li class="list-group-item">
-                    <b>Followers</b> <a class="pull-right">1,322</a>
+                    <b>No de Pacientes</b> <a class="pull-right">1,322</a>
                   </li>
                   <li class="list-group-item">
-                    <b>Following</b> <a class="pull-right">543</a>
+                    <b>Consultas</b> <a class="pull-right">543</a>
                   </li>
                   <li class="list-group-item">
-                    <b>Friends</b> <a class="pull-right">13,287</a>
+                    <b>Recetas Expedidas</b> <a class="pull-right">13,287</a>
                   </li>
                 </ul>
 
-                <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+                <!--<a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>-->
               </div>
               <!-- /.box-body -->
             </div>
@@ -49,39 +49,38 @@
             <!-- About Me Box -->
             <div class="box box-primary">
               <div class="box-header with-border">
-                <h3 class="box-title">About Me</h3>
+                <h3 class="box-title">Acerca de mí</h3>
               </div>
               <!-- /.box-header -->
               <div class="box-body">
-                <strong><i class="fa fa-book margin-r-5"></i> Education</strong>
+                <strong><i class="fa fa-hospital-o margin-r-5"></i> Hospital</strong>
+
+                <p class="text-muted">Los Angeles</p>
+
+                <hr>
+                <strong><i class="fa fa-mobile margin-r-5"></i> Celular</strong>
 
                 <p class="text-muted">
-                  B.S. in Computer Science from the University of Tennessee at Knoxville
+                  2221123318
                 </p>
 
                 <hr>
 
-                <strong><i class="fa fa-map-marker margin-r-5"></i> Location</strong>
+                <strong><i class="fa fa-map-marker margin-r-5"></i> Dirección</strong>
 
-                <p class="text-muted">Malibu, California</p>
-
-                <hr>
-
-                <strong><i class="fa fa-pencil margin-r-5"></i> Skills</strong>
-
-                <p>
-                  <span class="label label-danger">UI Design</span>
-                  <span class="label label-success">Coding</span>
-                  <span class="label label-info">Javascript</span>
-                  <span class="label label-warning">PHP</span>
-                  <span class="label label-primary">Node.js</span>
-                </p>
+                <p class="text-muted">Privada Santa Lucila 31-301, Cutlancingo, Puebla</p>
 
                 <hr>
 
-                <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
+                <strong><i class="fa fa-file-text margin-r-5"></i> Cédula Profesional</strong>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+                <p class="text-muted">12313BA</p>
+
+                <hr>
+
+                <strong><i class="fa fa-file-text-o margin-r-5"></i> RFC</strong>
+
+                <p "text-muted">BABR140985</p>
               </div>
               <!-- /.box-body -->
             </div>
@@ -91,122 +90,14 @@
           <div class="col-md-9">
             <div class="nav-tabs-custom">
               <ul class="nav nav-tabs">
-                <li class="active"><a href="#activity" data-toggle="tab">Activity</a></li>
-                <li><a href="#timeline" data-toggle="tab">Timeline</a></li>
-                <li><a href="#settings" data-toggle="tab">Settings</a></li>
+                <li class="active"><a href="#activity" data-toggle="tab">Actividad</a></li>
+                <li><a href="#timeline" data-toggle="tab">Línea de Tiempo</a></li>
+                <li><a href="#settings" data-toggle="tab">Ajustes</a></li>
               </ul>
               <div class="tab-content">
                 <div class="active tab-pane" id="activity">
                   <!-- Post -->
                   <div class="post">
-                    <div class="user-block">
-                      <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg" alt="user image">
-                          <span class="username">
-                            <a href="#">Jonathan Burke Jr.</a>
-                            <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
-                          </span>
-                      <span class="description">Shared publicly - 7:30 PM today</span>
-                    </div>
-                    <!-- /.user-block -->
-                    <p>
-                      Lorem ipsum represents a long-held tradition for designers,
-                      typographers and the like. Some people hate it and argue for
-                      its demise, but others ignore the hate as they create awesome
-                      tools to help create filler text for everyone from bacon lovers
-                      to Charlie Sheen fans.
-                    </p>
-                    <ul class="list-inline">
-                      <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a></li>
-                      <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
-                      </li>
-                      <li class="pull-right">
-                        <a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> Comments
-                          (5)</a></li>
-                    </ul>
-
-                    <input class="form-control input-sm" type="text" placeholder="Type a comment">
-                  </div>
-                  <!-- /.post -->
-
-                  <!-- Post -->
-                  <div class="post clearfix">
-                    <div class="user-block">
-                      <img class="img-circle img-bordered-sm" src="../../dist/img/user7-128x128.jpg" alt="User Image">
-                          <span class="username">
-                            <a href="#">Sarah Ross</a>
-                            <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
-                          </span>
-                      <span class="description">Sent you a message - 3 days ago</span>
-                    </div>
-                    <!-- /.user-block -->
-                    <p>
-                      Lorem ipsum represents a long-held tradition for designers,
-                      typographers and the like. Some people hate it and argue for
-                      its demise, but others ignore the hate as they create awesome
-                      tools to help create filler text for everyone from bacon lovers
-                      to Charlie Sheen fans.
-                    </p>
-
-                    <form class="form-horizontal">
-                      <div class="form-group margin-bottom-none">
-                        <div class="col-sm-9">
-                          <input class="form-control input-sm" placeholder="Response">
-                        </div>
-                        <div class="col-sm-3">
-                          <button type="submit" class="btn btn-danger pull-right btn-block btn-sm">Send</button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                  <!-- /.post -->
-
-                  <!-- Post -->
-                  <div class="post">
-                    <div class="user-block">
-                      <img class="img-circle img-bordered-sm" src="../../dist/img/user6-128x128.jpg" alt="User Image">
-                          <span class="username">
-                            <a href="#">Adam Jones</a>
-                            <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
-                          </span>
-                      <span class="description">Posted 5 photos - 5 days ago</span>
-                    </div>
-                    <!-- /.user-block -->
-                    <div class="row margin-bottom">
-                      <div class="col-sm-6">
-                        <img class="img-responsive" src="../../dist/img/photo1.png" alt="Photo">
-                      </div>
-                      <!-- /.col -->
-                      <div class="col-sm-6">
-                        <div class="row">
-                          <div class="col-sm-6">
-                            <img class="img-responsive" src="../../dist/img/photo2.png" alt="Photo">
-                            <br>
-                            <img class="img-responsive" src="../../dist/img/photo3.jpg" alt="Photo">
-                          </div>
-                          <!-- /.col -->
-                          <div class="col-sm-6">
-                            <img class="img-responsive" src="../../dist/img/photo4.jpg" alt="Photo">
-                            <br>
-                            <img class="img-responsive" src="../../dist/img/photo1.png" alt="Photo">
-                          </div>
-                          <!-- /.col -->
-                        </div>
-                        <!-- /.row -->
-                      </div>
-                      <!-- /.col -->
-                    </div>
-                    <!-- /.row -->
-
-                    <ul class="list-inline">
-                      <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a></li>
-                      <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
-                      </li>
-                      <li class="pull-right">
-                        <a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> Comments
-                          (5)</a></li>
-                    </ul>
-
-                    <input class="form-control input-sm" type="text" placeholder="Type a comment">
                   </div>
                   <!-- /.post -->
                 </div>
@@ -310,52 +201,64 @@
                 <div class="tab-pane" id="settings">
                   <form class="form-horizontal">
                     <div class="form-group">
-                      <label for="inputName" class="col-sm-2 control-label">Name</label>
+                      <label for="inputName" class="col-sm-2 control-label">Hospital</label>
 
                       <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputName" placeholder="Name">
+                        <select class="form-control select2" name="hospital" id="inputHospital" >
+                          <option selected="selected">Alabama</option>
+                          <option>Alaska</option>
+                          <option>California</option>
+                          <option>Delaware</option>
+                          <option>Tennessee</option>
+                          <option>Texas</option>
+                          <option>Washington</option>
+                        </select>
                       </div>
                     </div>
                     <div class="form-group">
-                      <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+                      <label for="inputName" class="col-sm-2 control-label">Nombre</label>
 
                       <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                        <input type="email" class="form-control" id="inputName" placeholder="Nombre">
                       </div>
                     </div>
                     <div class="form-group">
-                      <label for="inputName" class="col-sm-2 control-label">Name</label>
+                      <label for="inputEmail" class="col-sm-2 control-label">Celular</label>
 
                       <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputName" placeholder="Name">
+                        <input type="email" class="form-control" id="inputEmail" placeholder="Celular">
                       </div>
                     </div>
                     <div class="form-group">
-                      <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
+                      <label for="inputName" class="col-sm-2 control-label">Dirección</label>
 
                       <div class="col-sm-10">
-                        <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                        <input type="text" class="form-control" id="inputName" placeholder="Dirección">
                       </div>
                     </div>
                     <div class="form-group">
-                      <label for="inputSkills" class="col-sm-2 control-label">Skills</label>
+                      <label for="inputExperience" class="col-sm-2 control-label">Cédula Profesional</label>
 
                       <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
+                        <textarea class="form-control" id="inputExperience" placeholder="Cédula Profesional"></textarea>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="inputSkills" class="col-sm-2 control-label">RFC</label>
+
+                      <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputSkills" placeholder="RFC">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="inputSkills" class="col-sm-2 control-label">Subir Foto de Perfil</label>
+                      <div class="col-sm-10">
+                        <input type="file" id="exampleInputFile">
                       </div>
                     </div>
                     <div class="form-group">
                       <div class="col-sm-offset-2 col-sm-10">
-                        <div class="checkbox">
-                          <label>
-                            <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-danger">Submit</button>
+                        <button type="submit" class="btn btn-danger">Editar Ajustes</button>
                       </div>
                     </div>
                   </form>
